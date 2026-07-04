d4b0332e08da5d8d30b905ce3891eed9b147a582
kgyy1991 <166105560+kgyy1991@users.noreply.github.com>
2026-07-04 16:56:15 +0900

    Update frame.js

--- apps/gas-public/frame.js
+++ apps/gas-public/frame.js
@@ -1,10 +1,9 @@
 (function () {
-  document.getElementById('gas').src =
-  `https://script.google.com/macros/s/${
-    'AKfycbyTDeRXjoTRcFaMzxuAM2C5ekOFqKb1ARsJivwH56bpnP8CieuodPWAFpZ8d_EYh35azg'
-  }/exec${
-    location.pathname
-  }${
-    location.search
-  }`
+  var EXEC = 'https://script.google.com/macros/s/AKfycbyTDeRXjoTRcFaMzxuAM2C5ekOFqKb1ARsJivwH56bpnP8CieuodPWAFpZ8d_EYh35azg/exec';
+
+  // pathInfo を使わない：pathname を ?path= に畳み、search の各クエリも引き継ぐ
+  var params = new URLSearchParams(location.search);
+  params.set('path', location.pathname.replace(/^\//, ''));  // "station/ad13a9dd/family_monthly_plan"
+
+  document.getElementById('gas').src = EXEC + '?' + params.toString();
 })()
